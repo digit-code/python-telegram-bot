d8afe30140bb89be2983655b3c69dd3c73a8ba0e
Adama NIADA <a77272199@gmail.com>
2023-02-23 17:49:19 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Telegram Bot Vercel
+Telegram bot using based on https://github.com/python-telegram-bot/python-telegram-bot
+
+### Environment Variables
+```
+webhook = https://{your url}.vercel.app
+TOKEN = Telegram Bot Token
+```
+### Notes
+- Env name is case sensitive
+
---
+++ betmomo/bot.py
@@ -0,0 +1,43 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+from os import getenv
+
+# Define a few command handlers.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # await update.message.reply_html(text="hello world!")
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # await update.message.reply_html(text="help me!")
+    await update.message.reply_text(
+        "Use /start to test this bot. Use /clear to clear the stored data so that you can see "
+        "what happens, if the button data is not available. "
+    )
+async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Clears the callback data cache"""
+    context.bot.callback_data_cache.clear_callback_data()
+    context.bot.callback_data_cache.clear_callback_queries()
+    await update.effective_message.reply_text("All clear!")
+
+async def bot_tele(text):
+    # Create application
+    application = (
+        Application.builder().token(getenv("TOKEN")).build()
+    )
+
+    # Add handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("clear", clear))
+
+    # Start application
+    await application.bot.set_webhook(url=getenv("webhook"))
+    await application.update_queue.put(
+            Update.de_json(data=text, bot=application.bot)
+        )
+    async with application:
+        await application.start()
+        await application.stop()
